a21761b66eacd64831f4f9f1d266293f5cf5a39f
shailendra pratap <singhlalprat@gmail.com>
2026-02-16 18:39:23 +0530

    Create sync.js

---
+++ sync.js
@@ -0,0 +1,12 @@
+var a = 1078787;
+var b = 20987;
+
+function multiplyFn(x,y){
+    const result = a*b;
+    return result;
+}
+
+var c = multiplyFn(a,b);
+
+console.log("multiplication result is: ",c);
+  
