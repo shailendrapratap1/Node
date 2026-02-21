898fecf6c9764ad1ab132566926b00b39ef4cc18
shailendra pratap <singhlalprat@gmail.com>
2026-02-21 18:01:20 +0530

    database

---
+++ database.js
@@ -0,0 +1,10 @@
+// got to mongo db website 
+// create a free Mo cluster 
+// create a user 
+// get the connection string 
+// install mngo db compass
+ 
+const URI =" mongodb+srv://namaste:AcsTC2sf5Ny1qe6Q@namastenode.ndnjmam.mongodb.net/"
+
+
+
