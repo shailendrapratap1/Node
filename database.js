51998bd39f232a0ed8a36aab8ac7275cb15ccc79
shailendra pratap <singhlalprat@gmail.com>
2026-02-26 12:29:47 +0530

    Update database.js

--- database.js
+++ database.js
@@ -3,37 +3,64 @@
 // create a user 
 // get the connection string 
 // install mngo db compass
-const { MongoClient } = require('mongodb');
+import { MongoClient, ObjectId } from "mongodb";
 const url ="mongodb+srv://namaste:AcsTC2sf5Ny1qe6Q@namastenode.ndnjmam.mongodb.net/ "
 
 const client = new MongoClient(url);
 
-const dbName = 'helloworld'
-async function main(){
-    await client.connect();
-    console.log('Connected successfully to the server')
-    const db = client.db(dbName);
-    const collection = db.collection('user');
+await client.connect();
+
+const db = client.db("campus");
+const users = db.collection("users");
+
+await users.bulkWrite([
+  {
+    insertOne: {
+      document: {
+        _id: new ObjectId("67a1b2c3d4e5f6a7b8c9d0e1"),
+        name: "Tyrion Lannister",
+        email: "tyrion.lannister@example.com",
+        password: "$2b$12$UREFwsRUoyF0CRqGNK0LzO0HM/jLhgUCNNIJ9RJAqMUQ74crlJ1Vu"
+      }
+    }
+  },
+  {
+    updateOne: {
+      filter: { name: "Ned Stark" },
+      update: { $set: { email: "ned.stark.updated@example.com" } }
+    }
+  },
+  {
+    deleteOne: {
+      filter: { _id: new ObjectId("67a1b2c3d4e5f6a7b8c9d0e1") }
+    }
+  }
+]);
+// async function main(){
+//     await client.connect();
+//     console.log('Connected successfully to the server')
+//     const db = client.db(dbName);
+//     const collection = db.collection('user');
 
 
 
+
+// // const data = {
+// //     firstname:"deepika",
+// //     lastname:'padukone',
+// //     city:"mumbai",
+// //     phonenumber:"89898989899"
+// // }
 
 // const data = {
-//     firstname:"deepika",
-//     lastname:'padukone',
+//     firstname:"ranvir",
+//     lastname:"singh",
 //     city:"mumbai",
-//     phonenumber:"89898989899"
+//     phonenumber:"8786767589"
 // }
 
-const data = {
-    firstname:"ranvir",
-    lastname:"singh",
-    city:"mumbai",
-    phonenumber:"8786767589"
-}
-
-const insertResult = await collection.insertOne(data);
-console.log('Inserted documents =>', insertResult)
+// const insertResult = await collection.insertOne(data);
+// console.log('Inserted documents =>', insertResult)
 
 //     const findResult = await collection.find({}).toArray();
 //     console.log('Found documents =>', findResult)
@@ -43,13 +70,26 @@ console.log('Inserted documents =>', insertResult)
 
 // find all    
 
-const result = await collection.find({firstname:"deepika"}).count();
-console.log("result =>", result)
+// const result = await collection.find({firstname:"deepika"}).count();
+// console.log("result =>", result)
 
-return 'done.';
-}
+// return 'done.';
+// }
 
-main()
-.then(console.log)
-.catch(console.log)
-.finally(()=> client.close())
+// main()
+// .then(console.log)
+// .catch(console.log)
+// .finally(()=> client.close())
+
+
+
+
+
+
+
+
+
+
+
+
+
