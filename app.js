aec28213c78b52232055835a8930375a5da9c0e8
shailendra pratap <singhlalprat@gmail.com>
2026-03-01 11:48:49 +0530

    route handler

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
- require("./xyz.js") ;
-//   const {calculateSum} = require("./calculate/sum") ;  // one module into other 
-//  const {claculateMultiply} = require("./calculate/Multiply.js")
+//  require("./xyz.js") ;
+// //   const {calculateSum} = require("./calculate/sum") ;  // one module into other 
+// //  const {claculateMultiply} = require("./calculate/Multiply.js")
 
-const util = require("node:util")
+// const util = require("node:util")
 
- const data = require("./data.json")
- console.log(JSON.stringify(data))
-const {calculateSum,claculateMultiply} = require("./calculate")
-var a =10;
- var b =20; 
+//  const data = require("./data.json")
+//  console.log(JSON.stringify(data))
+// const {calculateSum,claculateMultiply} = require("./calculate")
+// var a =10;
+//  var b =20; 
 
- var name ="Namaste node js"
+//  var name ="Namaste node js"
  
- var x =100;
+//  var x =100;
 
-calculateSum(a,b)
- console.log(x)
- claculateMultiply(a,b)
-// import{x,multipliv} from './sum.js';
-// var name ="nanna"
-// var a=90;
-// var b=90;
+// calculateSum(a,b)
+//  console.log(x)
+//  claculateMultiply(a,b)
+// // import{x,multipliv} from './sum.js';
+// // var name ="nanna"
+// // var a=90;
+// // var b=90;
 
-// // var x=100;
-// multipliv(a,b)
-// console.log(x)  
+// // // var x=100;
+// // multipliv(a,b)
+// // console.log(x)  
+
+
+const express = require("express")
+
+const app = express()
+
+app.use("/user",(req,res)=>{
+    res.send("Route handler 1")
+})
+
+app.listen(7777,()=>{
+    console.log("server is successfully listening on the port 7777 ...")
+})
